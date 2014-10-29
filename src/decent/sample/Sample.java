fc74190ce5643b14b610e10d5bb117cb5eccb3f0
Philip Makedonski <philip.makedonski@gmail.com>
2014-10-29 14:43:25 +0100

    * refactored into a class and a method

--- src/decent/sample/Sample.java
+++ src/decent/sample/Sample.java
@@ -3,6 +3,11 @@ package decent.sample;
 public class Sample {
 
 	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.go();
+	}
+
+	private void go() {
 		System.out.println("Hello Java");
 		System.out.println("Hello World");
 		System.out.println("Hello Universe");
